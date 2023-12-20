6b6299d29d4601bbef25b1d7b7587f6c826b8b0d
amanDte21 <amandte21@gmail.com>
2023-12-20 12:39:33 +0530

    updated code

--- android/app/src/main/kotlin/com/downtownengineers/meeting_module2/MainActivity.kt
+++ android/app/src/main/kotlin/com/downtownengineers/meeting_module2/MainActivity.kt
@@ -2,14 +2,14 @@ package com.downtownengineers.meeting_module2
 
 import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
-import org.devio.flutter.splashscreen.SplashScreen
+//import org.devio.flutter.splashscreen.SplashScreen
 
 
 import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity: FlutterFragmentActivity() {
      override fun onCreate(savedInstanceState: Bundle?) {
-        SplashScreen.show(this, true) // here
+//        SplashScreen.show(this, true) // here
         super.onCreate(savedInstanceState)
     }
    
